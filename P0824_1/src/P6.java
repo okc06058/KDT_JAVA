8cd23c322ab4fc9a3f38eea8eb92fc531cf9bcb7
okc06058 <okc06058@naver.com>
2023-08-24 12:25:31 +0900

    0824

--- P0824_1/src/P6.java
+++ P0824_1/src/P6.java
@@ -3,6 +3,12 @@ public class P6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10;
+		while (a<20) {
+			System.out.println(a);
+			a++;
+		}
+		System.out.println("프로그램 종료");
 
 	}
 
---
+++ P0824_1/src/P9.java
@@ -0,0 +1,11 @@
+
+public class P9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int x = 1; x<=10; x++) {
+			System.out.println(x);
+		}
+	}
+
+}
